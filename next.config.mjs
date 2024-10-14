4ce80aa27b17b2369f4e66fc4d02a3a93cecbeb4
Ammad-Cherrybyte <63580683+ammad-ahmed1@users.noreply.github.com>
2024-10-15 03:37:34 +0500

    disabled-eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["cdn.imagin.studio", "www.google.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds
+  },
 };
 
 export default nextConfig;
